e605b919f24472afd445998fa9f712c870a56eec
Anthony Campos <anthonydevelops@gmail.com>
2020-06-02 11:27:22 -0700

    updated config v3

--- cmpConfig.js
+++ cmpConfig.js
@@ -1,3 +1,3 @@
 window.cmpConfig = JSON.parse(
-  '{"countryCode":"US","id":10024,"version":"3","legalBases":{"feature":[1,2,3],"purpose":{"consent":[1,2,3,4,5,6,7,9,10]},"specialPurpose":[1,2]},"legalName":"Cosmic Shovel, Inc.","policyUrl":"https://camelcamelcamel.com/privacy","isServiceSpecific":true,"vendors":[24,13,10,91,69,144],"bannerLayout":true,"gdprAppliesGlobally":true}'
+  '{"legalName":"Cosmic Shovel, Inc.","policyUrl":"https://camelcamelcamel.com/privacy","isServiceSpecific":true,"legalBases":{"feature":[1,2,3],"purpose":{"legitimateInterest":[],"consent":[1,2,3,4,5,6,7,9,10]},"specialPurpose":[1,2],"specialFeature":[]},"vendors":[24,13,10,91,69,144],"gdprAppliesGlobally":true,"countryCode":"AA","id":10024,"bannerLayout":true}'
 );
